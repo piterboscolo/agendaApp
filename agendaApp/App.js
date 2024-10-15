70173746a2d2a644c576063839e1ed1646c1c227
piterboscolo <piterboscolo>
2024-10-15 20:55:17 -0300

    first commit

---
+++ agendaApp/App.js
@@ -0,0 +1,11 @@
+import Login from "./src/screens/login/login.jsx";
+import Account from "./src/screens/account/account.jsx";
+
+function App() {
+
+  return <>
+    <Account />
+  </>;
+}
+
+export default App;
